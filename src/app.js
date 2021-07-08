386f816dd85b3df50afd9a17f8510e70089b6c43
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-07-08 21:39:07 +0300

    adds all functionality except flipping

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
 import galleryItems from "./data/gallery-data.js";
 
-console.log(galleryItems);
+const refs = {
+  galleryListEl: document.querySelector(".js-gallery"),
+  modalEl: document.querySelector(".js-lightbox"),
+  modalCloseBtn: document.querySelector("[data-action = close-lightbox]"),
+  imageOriginalEl: document.querySelector(".lightbox__image"),
+  overlayEl: document.querySelector(".lightbox__overlay"),
+};
+
+const rendersGalleryMarkup = () => {
+  const arrOfStr = galleryItems.map(({ preview, original, description }) => {
+    return `<li class = gallery__item><a class = gallery__link><img class = gallery__image src = '${preview}' data-source = '${original}' alt = '${description}'></a>    </li>`;
+  });
+
+  const str = arrOfStr.join("");
+
+  refs.galleryListEl.insertAdjacentHTML("afterbegin", str);
+};
+
+const onImageClick = function (e) {
+  refs.modalEl.classList.add("is-open");
+  refs.imageOriginalEl.src = e.target.dataset.source;
+  refs.imageOriginalEl.alt = e.target.alt;
+};
+
+const onModalBtnClick = function () {
+  refs.modalEl.classList.remove("is-open");
+
+  refs.imageOriginalEl.src = "";
+};
+
+const onEscPress = (e) => {
+  if (e.key === "Escape") {
+    onModalBtnClick();
+  }
+};
+
+refs.galleryListEl.addEventListener("click", onImageClick);
+refs.modalCloseBtn.addEventListener("click", onModalBtnClick);
+refs.overlayEl.addEventListener("click", onModalBtnClick);
+window.addEventListener("keydown", onEscPress);
+
+rendersGalleryMarkup();
